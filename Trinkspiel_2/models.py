1e9d1804037d5e7cd947537dae9a820c4e68410f
Hendrik <test>
2017-01-28 18:29:33 +0100

    model changes

--- Trinkspiel_2/models.py
+++ Trinkspiel_2/models.py
@@ -2,20 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Game(models.Model):
+    difficulty = models.IntegerField = 1
+
 class Player(models.Model):
     name = models.CharField(max_length=100)
     sips_game = models.IntegerField = 0
     shots_game = models.IntegerField = 0
-    sips_total = models.IntegerField = 0
-    shots_total = models.IntegerField = 0
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
     gender = (
         ('male', 'männlich'),
         ('female', 'weiblich'),
     )
-
-class Game(models.Model):
-    difficulty = models.IntegerField = 1
-    players = models.ManyToManyField(Player)
 
 class Task(models.Model):
     target = Player
