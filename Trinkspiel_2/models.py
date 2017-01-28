5974805201a931a55936a93ddd68ce30ae2cf3dd
Hendrik <test>
2017-01-28 18:21:21 +0100

    model changes

--- Trinkspiel_2/models.py
+++ Trinkspiel_2/models.py
@@ -2,20 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class game(models.Model):
-    difficulty = models.IntegerField = 1
-
-
-class player(models.Model):
+class Player(models.Model):
     name = models.CharField(max_length=100)
-    sip_counter = models.IntegerField(null=True)
-    shot_counter = models.IntegerField(null=True)
+    sips_game = models.IntegerField = 0
+    shots_game = models.IntegerField = 0
+    sips_total = models.IntegerField = 0
+    shots_total = models.IntegerField = 0
     gender = (
         ('male', 'männlich'),
         ('female', 'weiblich'),
     )
-    # game = game
 
-class task(models.Model):
-    target = player
+class Game(models.Model):
+    difficulty = models.IntegerField = 1
+    players = models.ManyToManyField(Player)
+
+class Task(models.Model):
+    target = Player
     task = models.CharField(max_length=500)
